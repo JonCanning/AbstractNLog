916366e0d0236db149d279796474d1e5f82d499c
Jon Canning <jon.canning@gmail.com>
2013-05-28 15:28:16 +0100

    updated assemblyinfo

--- src/AbstractNLog/Properties/AssemblyInfo.cs
+++ src/AbstractNLog/Properties/AssemblyInfo.cs
@@ -1,18 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AbstractNLog")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("A simple NLog Syslog abstraction")]
+[assembly: AssemblyCompany("Jon Canning")]
 [assembly: AssemblyProduct("AbstractNLog")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
